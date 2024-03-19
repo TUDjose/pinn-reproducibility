07d428dc02f8cd00484d0c4759187b11bbd3d67d
Lucas Van Mol <lucas.vanmol@gmail.com>
2024-03-19 15:18:37 +0100

    Add LBFGS

--- pendulum/main.py
+++ pendulum/main.py
@@ -43,13 +43,15 @@ m = 1
 l = 1
 g = 9.8
 
-n_adam = 1000
-
+n_adam = 5000
+n_domain = 1000
+device = "cpu"
 
 def constraint_loss(theta, theta_t, theta_tt, torq, t):
     # need to add constraint for theta_t[0] = 0 but it doesn't seem to work very well
     # have also set the goal to 0 for now
-    return (theta[0] - torch.pi/3)**2 + torch.mean(torch.norm(torq, p=2)**2) #+ theta
+    # print(theta, theta_t)
+    return 10*(theta[0] - torch.pi/3)**2 + torch.mean(torch.norm(torq, p=2)**2) + (theta_t[0][0])**2 
 
 theta_t = None
 theta_tt = None
@@ -77,19 +79,20 @@ def pinn_loss(theta, torq, t, loss_weights, loss_fns):
 
 net = FNN([1] + [64] * 3 + [n_output])
 
-time_domain = torch.linspace(tmin, tmax, 100)
-time_domain = time_domain.reshape((100, 1))
+time_domain = torch.linspace(tmin, tmax, n_domain)
+time_domain = time_domain.reshape((n_domain, 1))
 time_domain.requires_grad = True
 
 def train(model, optimizer, steps):
     # set model to training mode
     model.train()
 
+    print("Adam..")
     for n in range(steps):
         # Forward
         u = model(time_domain)
         theta, torq = u[:, 0], u[:, 1]
-        loss = pinn_loss(theta, torq, time_domain, loss_weights=[10, 1], loss_fns=[constraint_loss, physics_loss])
+        loss = pinn_loss(theta, torq, time_domain, loss_weights=[1, 1], loss_fns=[constraint_loss, physics_loss])
 
         # Backprop
         loss.backward()
@@ -98,24 +101,46 @@ def train(model, optimizer, steps):
         if n % 100 == 0:
             print(loss)
 
+    # Define closure for L-BFGS
+    def closure():
+        optimizer.zero_grad()
+        u = model(time_domain)
+        theta, torq = u[:, 0], u[:, 1]
+        loss = pinn_loss(theta, torq, time_domain, loss_weights=[1, 1], loss_fns=[constraint_loss, physics_loss])
+        loss.backward()
+        return loss
+    
+    print("L-BFGS..")
+    optimizer = torch.optim.LBFGS(model.parameters(), lr=1e-1)
+    try:
+        for n in range(steps):
+            optimizer.step(closure)
+            loss = closure()
+            print(loss)
+    except KeyboardInterrupt:
+        pass
+
 def plot_output(model):
     model.eval()
 
     with torch.no_grad():
-        u = [model(t.reshape(1)) for t in time_domain]
+        u = [model(t.reshape(1)).to(device=device) for t in time_domain]
     
     print(u)
     plt.plot(time_domain.detach().numpy(), [y[0] for y in u], label="theta")
     plt.plot(time_domain.detach().numpy(), theta_t.reshape((-1, 1)).detach().numpy(), label="theta_t")
+    plt.plot(time_domain.detach().numpy(), theta_tt.reshape((-1, 1)).detach().numpy(), label="theta_tt")
 
     plt.plot(time_domain.detach().numpy(), [torch.tanh(y[1]) * max_torq for y in u], label="torque")
     plt.legend()
     plt.show()
 
 
+time_domain = time_domain.to(device=device)
+net.to(device=device)
 print("Training..")
 
-train(net, torch.optim.Adam(net.parameters(), lr=2e-2), n_adam)
+train(net, torch.optim.Adam(net.parameters(), lr=2e-3), n_adam)
 
 print("Plotting..")
 
